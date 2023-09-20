d5584ab3299cc0de4fdc60cc9a11e2293a0c6f77
MeliEiros <meelieiros@gmail.com>
2023-09-19 23:11:09 -0300

    ocultar/mostrar section nueva operacion y balance

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//alert (`Hola, somos Meli & Ceci`)
+// balance y nueva operacion ocultar-desocultar
+const seeOperation =document.getElementById("see-operation");
+
+const balance =document.getElementById("balance");
+const newOperation =document.getElementById("see-newoperation");
+
+const operation = () => { 
+    balance.classList.remove("is-hidden");
+    newOperation.classList.add("is-hidden");
+} 
+
+const hidden = () => { 
+   newOperation.classList.remove("is-hidden");
+   balance.classList.add("is-hidden");
+} 
+
+seeOperation.addEventListener("click" , () => hidden());
+
+const clickBalance =document.getElementById("see-balance");
+
+const click = () => { 
+    newOperation.classList.add("is-hidden")
+    balance.classList.remove("is-hidden");
+}
+
+clickBalance.addEventListener("click" , () => click());
+
+
