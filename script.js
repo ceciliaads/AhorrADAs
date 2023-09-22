2d86949f5922e3f7b8ce77dfb179536a1188f26c
MeliEiros <meelieiros@gmail.com>
2023-09-21 21:18:44 -0300

    resuelto mostrar/ocultar filtros.Comienzo de la parte nueva operacion

--- script.js
+++ script.js
@@ -25,4 +25,41 @@ const click = () => {
 
 clickBalance.addEventListener("click" , () => click());
 
+//ocultar filtros 
 
+const yesFiltros =document.getElementById("see-filtros"); 
+const boxFiltros =document.getElementById("filtros");
+const noFiltros =document.getElementById("toggle-filtros");
+
+const mostrarFiltros = () => { 
+    boxFiltros.classList.remove("is-hidden");
+    yesFiltros.classList.add("is-hidden");
+    noFiltros.classList.remove("is-hidden");
+}
+
+const ocultarFiltros = () => { 
+    yesFiltros.classList.remove("is-hidden");
+    noFiltros.classList.add("is-hidden");
+    boxFiltros.classList.add("is-hidden");
+}
+
+noFiltros.addEventListener("click" , () => ocultarFiltros());
+yesFiltros.addEventListener("click" , () => mostrarFiltros());
+
+//Nueva operacion 
+
+const listaDeOperac = [];
+
+const addName = () => { 
+let newOp = document.getElementById("description-input").value;
+listaDeOperac.push(newOp);
+let li = document.createTextNode('${newOp}');
+let liItem = document.createElement('li');
+liItem.appendChild(li);
+
+console.log(listaDeOperac);
+}
+
+const agregar= document.getElementById("add-operation");
+
+agregar.addEventListener("click" , () => addName());
